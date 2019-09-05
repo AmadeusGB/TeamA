518a7e71b54ccf99924d7d940bfccdac17a5bcfa
yi-kuai-lian-xi <40764905+yi-kuai-lian-xi@users.noreply.github.com>
2019-09-05 17:29:11 +0800

    Add files via upload

---
+++ projects/linked_item.rs
@@ -0,0 +1,26 @@
+use support::{StorageMap,Parameter};
+use runtime_primitives::traits::Member;
+use parity_codec::{Encode, Decode};
+
+#[cfg_attr(feature = "std",derive(Debug, PartialEq, Eq))]
+#[derive(Encode, Decode)]
+pub struct LinkedItem<Item> {
+    pub prev: Option<Item>,
+    pub next: Option<Item>,
+}
+
+pub struct LinkedList<Storage, Key, Item>(rstd::marker::PhantomData<(StorageMap))
+
+impl<Storage, Key, Value> LinkedList<Storage, Key, Value> where
+    Value: Parameter + Member + Copy,
+    Key: Parameter,
+    Storage: StorageMap<(Key, Option<Value>), LinkedItem<Value>, Query = Option<Item>>
+{
+    fn read_head(key: &Key) -> LinkedItem<Value> {
+        Self::read(key, None)
+    }    
+    
+    fn write_head(account: &Key, item: LinkedItem<Value>) {
+        Self::write(account, None, item);
+    }
+}
